dae6fdc3abf80ee026c87b116111fd917d23cbfb
cd <cdiede@t-online.de>
2019-08-14 16:13:41 +0200

    Logging added, create only direct child components (bugfix)

--- src/Mojito.js
+++ src/Mojito.js
@@ -34,34 +34,53 @@ var Mojito = (function() {
   Mojito.utils = {};
 
   /**
+   * Logging info.
+   */
+  Mojito.logging = Boolean(JSON.parse(window.sessionStorage.getItem("MojitoLogging")));
+
+  /**
    * Render all
    */
   Mojito.prototype.render = function() {
     this.renderComponent();
-    this.createComponents();
+    this.createChildComponents(this.getChildComponentElements());
   };
 
   /**
    * Render only the component itself
    */
   Mojito.prototype.renderComponent = function() {
+    if (Mojito.logging) console.log("Render component " + this.selector);
     this.attributes = this.element.dataset;
     this.element.innerHTML = this.template(this.data, this.attributes);
   };
 
   /**
-   * Create all direct child components
+   * Get all child components
    */
-  Mojito.prototype.createComponents = function() {
-    var elements = this.element.querySelectorAll("[data-mojito-comp]");
-    elements = Array.prototype.slice.call(elements); // IE workaround
-    var _this = this;
+  Mojito.prototype.getChildComponentElements = function() {
+    var childComponentElements = this.element.querySelectorAll(
+      "[data-mojito-comp]"
+    );
+    childComponentElements = Array.prototype.slice.call(childComponentElements); // IE workaround
+    return childComponentElements;
+  };
 
-    elements.forEach(function(element) {
+  /**
+   * Create child components.
+   */
+  Mojito.prototype.createChildComponents = function(childComponentElements) {
+    var _this = this;
+    childComponentElements.forEach(function(element) {
       var componentName = element.dataset.mojitoComp || "";
       var componentId = element.dataset.mojitoId || null;
       var compontentSelector = '[data-mojito-comp="' + componentName + '"]';
-      if (componentId) compontentSelector += '[data-mojito-id="' + componentId + '"]';
+      if (componentId)
+        compontentSelector += '[data-mojito-id="' + componentId + '"]';
+      if (Mojito.logging)
+        console.log(
+          _this.selector + " creates child component " + compontentSelector
+        );
 
       // Create child component
       var component = Mojito.components[componentName];
@@ -81,8 +100,9 @@ var Mojito = (function() {
     // 2. Grab element from DOM
     this.element = document.querySelector(this.selector);
 
-    // 3. Render the components
+    // 3. Render the component and save the next child components
     this.renderComponent();
+    var childComponentElements = this.getChildComponentElements();
 
     // 4. Call created function of the component
     this.created(
@@ -93,7 +113,7 @@ var Mojito = (function() {
     );
 
     // 5. Create child components
-    this.createComponents();
+    this.createChildComponents(childComponentElements);
   };
 
   return Mojito;
